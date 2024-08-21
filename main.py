91ab53dd187bde62f6a2a255e84ae0708c437492
wangye <wangye@i-i.ai>
2024-08-21 14:37:39 +0800

    init

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import io
 import os
+import shutil
 from pathlib import Path
-from yaml import load, dump, Loader
-from asyncinotify import Inotify, Mask
-import asyncio
+from yaml import load, Loader
+
+from inotifyrecursive import INotify, flags
 
 
 class Rule:
@@ -13,9 +14,16 @@ class Rule:
     extensions: list
 
     def __init__(self, rule):
-        self.move_to = rule.get('move_to')
+        self.move_to = os.path.expandvars(rule.get('move_to'))
         self.extensions = rule.get('extensions')
         self.name = rule.get('name')
+
+    def match(self, path):
+        for ext in self.extensions:
+            if path.is_file():
+                if path.suffix == ext:
+                    return True
+        return False
 
 
 class Watcher:
@@ -24,7 +32,6 @@ class Watcher:
     rules: list
 
     def __init__(self):
-        # {'name': 'test', 'rules': [{'name': 'movie', 'dir': '${HOME}/Downloads', 'move_to': '${HOME}/Movie', 'extensions': ['.mp4', '.mkv']}]}
         stream = io.open("config.yaml", "r")
         config = load(stream, Loader)
         self.name = config.get("name")
@@ -32,24 +39,24 @@ class Watcher:
         rules = config.get("rules")
         self.rules = [Rule(rule) for rule in rules]
 
-    async def start(self):
-        with Inotify() as inotify:
+    def start(self):
+        with INotify() as inotify:
             for d in self.dirs:
                 path = os.path.expandvars(d)
-                inotify.add_watch(path, Mask.MODIFY | Mask.CREATE)
+                inotify.add_watch_recursive(path, flags.CREATE)
                 print(f"watching {d}")
-            # Iterate events forever, yielding them one at a time
-            async for event in inotify:
-                # Events have a helpful __repr__.  They also have a reference to
-                # their Watch instance.
-                #print(event)
-                path = event.path
-                print(path.absolute())
-                print(path.cwd())
-                # the contained path may or may not be valid UTF-8.  See the note
-                # below
-                #print(repr(event.path.suffix))
+            while True:
+                for event in inotify.read():
+                    for rule in self.rules:
+                        try:
+                            path = Path(Path(inotify.get_path(event.wd)) / Path(event.name))
+                            if rule.match(path):
+                                print(f'{rule.name} matched :{path} , file moved to {rule.move_to}')
+                                shutil.move(path, rule.move_to)
+                        except BaseException as e:
+                            print(e)
+                            continue
 
 
 if __name__ == '__main__':
-    asyncio.run(Watcher().start())
+    Watcher().start()
