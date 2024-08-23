166ef841ed9ae93e462356bae043398f33350394
wangye <wangye@i-i.ai>
2024-08-23 18:23:13 +0800

    first version

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+#
+#   author:qiuyuan
+#
+#
 import io
 import os
+import re
 import shutil
 from pathlib import Path
 from yaml import load, Loader
@@ -9,54 +14,135 @@ from inotifyrecursive import INotify, flags
 
 class Rule:
     name: str
-    dir: str
+    base: list
+    rm_dir_if_empty: bool
+    recursion: bool
     move_to: str
     extensions: list
+    regulars: list
+    rename: list
 
-    def __init__(self, rule):
+    def __init__(self, rule, base):
         self.move_to = os.path.expandvars(rule.get('move_to'))
         self.extensions = rule.get('extensions')
         self.name = rule.get('name')
+        self.rm_dir_if_empty = rule.get('rm_dir_if_empty')
+        self.rm_ignore = rule.get('rm_ignore')
+        self.recursion = rule.get('recursion')
+        self.base = base.copy()
+        self.base.append('/')  # 防止死循环
+        self.overwrite = rule.get('overwrite')
+        self.regulars = rule.get('regulars')
+        self.rename = rule.get('rename')
 
-    def match(self, path):
+    def handle(self, path):
+        if self.__match(path):
+            self.__move(path, self.__rename(path.name))
+
+    def __rm_ignore_match(self, name):
+        if len(self.rm_ignore) > 0:
+            for r in self.rm_ignore:
+                if r in name:
+                    return True
+
+    def __rm_dir(self, path):
+        if self.rm_dir_if_empty:
+            dir_path = path.parent
+            if str(dir_path) not in self.base:
+                scandir = os.scandir(dir_path)
+                for f in scandir:
+                    if not self.__rm_ignore_match(f.name):
+                        print(f"当前文件夹不为空，存在文件{f.name}，不做删除")
+                        return
+                dir_path.rmdir()
+                print(f"delete dir : {dir_path}")
+
+    def __move(self, path, new_name):
+        if self.move_to is None:
+            print("未定义目标目录")
+            return
+        new_path = os.path.join(self.move_to, new_name)
+        if not self.overwrite:
+            suffix = path.suffix
+            i = 0
+            while os.path.exists(new_path):
+                i += 1
+                print(f" 路径 {new_path} 已存在，生成新路径")
+                new_path = os.path.join(self.move_to, f"{new_name.split(suffix)[0]}-({i}){suffix}")
+        print(f"{path}  move to {new_path}")
+        shutil.move(path, new_path)
+        self.__rm_dir(path)
+
+    def __match_extensions(self, path):
+        if self.extensions is None:
+            return True
         for ext in self.extensions:
             if path.is_file():
                 if path.suffix == ext:
                     return True
         return False
 
+    def __match_regulars(self, path):
+        if self.regulars is None:
+            return True
+        for regular in self.regulars:
+            if re.search(regular, path.name):
+                return True
+        return False
+
+    def __rename(self, name):
+        if self.rename is None:
+            return name
+        new_name = name
+        for rule in self.rename:
+            new_name = new_name.replace(rule['ori'], rule['new'])
+        return new_name
+
+    def __match(self, path):
+        return self.__match_extensions(path) and self.__match_regulars(path)
+
 
 class Watcher:
     name: str
-    dirs: list
+    dirs: list = []
     rules: list
 
     def __init__(self):
         stream = io.open("config.yaml", "r")
         config = load(stream, Loader)
         self.name = config.get("name")
-        self.dirs = config.get("dirs")
+        dirs = config.get("dirs")
+        if len(dirs) == 0:
+            raise Exception("未配置目录")
+        for d in dirs:
+            d = os.path.expandvars(d)
+            if not os.path.exists(d):
+                raise Exception(f"目录:{d} 不存在")
+            self.dirs.append(d)
         rules = config.get("rules")
-        self.rules = [Rule(rule) for rule in rules]
+        self.rules = [Rule(rule, self.dirs) for rule in rules]
 
-    def start(self):
+    def __handle_rule(self, path):
+        for rule in self.rules:
+            rule.handle(path)
+
+    def start(self, startup):
+        if startup:
+            for path in self.dirs:
+                print("1")
         with INotify() as inotify:
-            for d in self.dirs:
-                path = os.path.expandvars(d)
+            for path in self.dirs:
                 inotify.add_watch_recursive(path, flags.CREATE)
-                print(f"watching {d}")
+                print(f"watching {path}")
             while True:
                 for event in inotify.read():
-                    for rule in self.rules:
-                        try:
-                            path = Path(Path(inotify.get_path(event.wd)) / Path(event.name))
-                            if rule.match(path):
-                                print(f'{rule.name} matched :{path} , file moved to {rule.move_to}')
-                                shutil.move(path, rule.move_to)
-                        except BaseException as e:
-                            print(e)
-                            continue
+                    path = Path(Path(inotify.get_path(event.wd)) / Path(event.name))
+                    try:
+                        self.__handle_rule(path)
+                    except BaseException as e:
+                        print(e)
+                        continue
 
 
 if __name__ == '__main__':
-    Watcher().start()
+    Watcher().start(1)
